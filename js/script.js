88072a336dab4281098f9cdfefe7b66148d98b1e
autohugo <autohugo@autohugo.local>
2018-07-12 11:57:29 +0000

    updating site [ci skip]

---
+++ js/script.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    'use strict';
+
+    var template, typeaheadOnClickCallback, typeaheadResultCallback,
+        typeaheadReadyCallback, adaptLocation;
+
+    template = [
+        '<span>{{package}} ',
+        '<span class="arch">~{{arch}}</span>',
+        '</span> ',
+        '<span style="color:green;" class="kk-suggest-detail"> ',
+        '{{repository}}',
+        '</span>'
+    ].join('');
+
+    typeaheadOnClickCallback = function($searchInput, $resultItem, metadata) {
+        var modal;
+
+        adaptLocation(metadata.package);
+        $('.js-typeahead').attr('value', metadata.package);
+
+        modal = $('#addRepository');
+        modal.find('.package').text(metadata.package);
+        modal.find('a.package').attr('href', location.href);
+        modal.find('#PackageName').attr('value', location.href);
+        modal.find('.repository').text(metadata.repository);
+        modal.find('.packagearch').text(metadata.arch);
+        modal.modal();
+    };
+
+    typeaheadResultCallback = function(node, query, result) {
+        if (query === '') { return; }
+
+        if (result.length === 0) {
+            $('.result-container').text('No results found :-(');
+        } else {
+            $('.result-container').text('');
+        }
+    };
+
+    typeaheadReadyCallback = function(data) {
+        var packageName, packageMetadata;
+
+        if (location.hash !== '') {
+            packageName = location.hash.slice(1);
+
+            packageMetadata = data.filter(function(metadata) {
+                return metadata.package == packageName;
+            });
+
+            if (packageMetadata.length > 0) {
+                typeaheadOnClickCallback(null, null, packageMetadata[0]);
+            }
+        }
+    };
+
+    adaptLocation = function(packagename) {
+        location.hash = '#' + packagename;
+    };
+
+    window.parsePackages = function(data) {
+        $('#searchpkg .js-typeahead').typeahead({
+            order: "asc",
+            dynamic: !0,
+            delay: 500,
+            source: {
+                packages: {
+                    display: "package",
+                    data: data,
+                    template: template
+                }
+            },
+            callback: {
+                onClick: typeaheadOnClickCallback,
+                onResult: typeaheadResultCallback,
+                onReady: function(node) {
+                    typeaheadReadyCallback(data);
+                }
+            }
+        });
+
+    };
+
+    // JSONP calls parsePackages with payload after finish
+    $.ajax({
+        url: "https://mirror3.mirror.garr.it/mirrors/sabayonlinux/sbi/metadata/metadata.json",
+        dataType: "jsonp",
+    });
+
+    if ('execCommand' in document) {
+        $('#RepositoryLabel').on('click', function() {
+            var $copyInput;
+
+            $copyInput = $('#PackageName');
+            $copyInput.attr('type', 'text');
+            $copyInput.select();
+            document.execCommand('copy');
+            $copyInput.attr('type', 'hidden');
+        });
+    }
+});
